04b3b43f57f2f951d778325da6c1c21184219931
James Dunnigan <james@ctrl-alt-delete.ca>
2018-04-02 23:10:16 -0700

    setup

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by James Dunnigan on 2018-04-02.
+//  Copyright © 2018 James Dunnigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
